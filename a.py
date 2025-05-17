7aefc61d725ffe2a485f0b0b6f59e373a1962c1d
deepaknandeshwar17 <deepaknandeshwar17@gmail.com>
2025-05-17 18:53:11 +0530

    Adding the torch part

---
+++ a.py
@@ -0,0 +1,19 @@
+# name = input("Name : ")
+# usn = input("usn")
+# m1 = int(input("m1"))
+# m2 = int(input("m2"))
+# m3 = int(input("m3"))
+
+# total = m1 + m2 + m3
+# percentage = round((total/300)*100)
+
+
+# print("name of studnt:",  name)
+
+age = int(input())
+
+
+if age >=18:
+    print("ok")
+else:
+    print("no")
